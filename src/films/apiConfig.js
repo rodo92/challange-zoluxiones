2cb3be2a08a199bde64fdf2a74926191b398cb3f
Rodolfo Crisanto <crisanto.rosas.22@gmail.com>
2022-02-23 00:02:51 -0500

    Feat: config api

---
+++ src/films/apiConfig.js
@@ -0,0 +1,5 @@
+const  SWAPI_GET_ALL_FILM_URL = "https://swapi.py4e.com/api/films/";
+const  SWAPI_GET_FILM_URL = (idFilms) => (`https://swapi.py4e.com/api/films/${idFilms}/?format=json`);
+
+exports.SWAPI_GET_ALL_FILM_URL = SWAPI_GET_ALL_FILM_URL;
+exports.SWAPI_GET_FILM_URL = SWAPI_GET_FILM_URL;
