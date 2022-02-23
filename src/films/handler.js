7ba3b382fc587db67ca5fb28d0fb4c9d01424fe0
Rodolfo Crisanto <crisanto.rosas.22@gmail.com>
2022-02-23 00:35:17 -0500

    Feat: test consumer api local

---
+++ src/films/handler.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const axios = require('axios');
+const {SWAPI_GET_ALL_FILM_URL, SWAPI_GET_FILM_URL} =  require('./apiConfig.js');
+const {filmParse} = require("./model.js");
+
+const getUrl = (idFilm) => {
+    return  idFilm? SWAPI_GET_FILM_URL(idFilm) : SWAPI_GET_ALL_FILM_URL;
+}
+
+module.exports.listFilm =  async (event) => {
+    
+    const {idFilm} = event.pathParameters ? event.pathParameters: {idFilm:''};
+
+    const filmAPIUrl = getUrl(idFilm);
+    const params = {
+        "params": {"format": "json", ...event.queryStringParameters  }
+    }
+
+    try{
+        let data;
+        const response = await axios.get(filmAPIUrl, params);
+
+        if (idFilm){
+
+            data  = filmParse(response.data);
+            
+
+        } else {
+            response.data.results = response.data.results.map(people => (filmParse(people)));
+            data  = response.data;
+        }
+        
+        return {
+
+            'statusCode': 200,
+            'headers': {'Content-Type': 'application/json'},
+            'body': JSON.stringify(data)
+        };
+
+  } catch (error){
+       
+        return {
+            'statusCode': error.response.status,
+            'body': JSON.stringify( error.response.statusText)
+        };
+       
+  }
+};
+
+module.exports.getUrl = getUrl;
