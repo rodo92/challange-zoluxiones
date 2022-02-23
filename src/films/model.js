2a8c73633f81cf46a13d64286b95d96fa8fa3376
Rodolfo Crisanto <crisanto.rosas.22@gmail.com>
2022-02-23 00:09:41 -0500

    Feat: make model structuc

---
+++ src/films/model.js
@@ -0,0 +1,29 @@
+const filmFieldsParse = {
+    "title": "titulo",
+    "episode_id": "numero_episodio",
+    "opening_crawl": "texto_apertura",
+    "director": "director",
+    "producer": "productp",
+    "release_date": "fecha_estreno",
+    "species": "especies",
+    "starships": "naves_estelares",
+    "vehicles": "vehiculos",
+    "characters": "caracteres",
+    "planets": "planetas",
+    "url": "url",
+    "created": "fecha_creacion",
+    "edited": "fecha_edicion"
+
+}
+
+const filmParse = (film) => {
+    
+    return Object.keys(film).reduce((obj, field) => {
+    
+        const parse_field = filmFieldsParse[field];
+        obj[parse_field] = film[field];
+        return obj;
+    }, {});
+};
+
+exports.filmParse = filmParse;
